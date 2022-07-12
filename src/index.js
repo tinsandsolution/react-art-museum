c8c6731293df7df21c9340a4f0f8a647aecab0b5
Standard <oanlara8@gmail.com>
2022-07-12 17:41:38 -0600

    init

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 
+const Root = () => {
+  return (
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  )
+  //return <h1>Hello from Root</h1>
+}
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>,
   document.getElementById('root')
 );
